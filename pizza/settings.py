53486a865bf2e20294614c469d060e881539ad5b
Tra My <lethitramy27@gmail.com>
2020-02-01 17:12:23 +0700

    Update database configuration for Heroku

--- pizza/settings.py
+++ pizza/settings.py
@@ -155,3 +155,8 @@ APPEND_SLASH=False
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
